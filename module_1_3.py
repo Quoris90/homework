15a27df3b98a73464e1c5e6ec65ef1836f2d747c
Quoris90 <rabdullin90@gmail.com>
2024-11-12 20:16:19 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,22 @@
+name = "Quoris"
+print('name: ' + name)
+age = int(34)
+print('age: ' + str(age))
+new_age = (22)
+print('New age : ' + str(new_age))
+student = True
+print(student)
+
+#ver 2.0
+print("""
+I'll try again more interesting""")
+
+name2 = input("Just write your name? ")
+print(name2)
+age2 = int(input("How old are you? "))
+print(age2)
+new_age2 = int(input('sign me next age... '))
+print(new_age2)
+if type(age2) == type(new_age2): print("Student: True")
+else: print("Student: False")
+
