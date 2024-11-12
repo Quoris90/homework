939ec207e37d00354f12935fd802b78063687fea
Quoris90 <rabdullin90@gmail.com>
2024-11-12 20:55:01 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+value_homework = 12
+spendhour = 1.5
+names_lesson = "Python"
+time4one_task = spendhour/value_homework
+print('Курс:',names_lesson, "всего задач:", value_homework, "затрачено часов:", spendhour, "средне время выполнения", time4one_task)
+print(f'Курс:{names_lesson} всего задач: {value_homework} затрачено часов {spendhour} среднее время выполнения {time4one_task}')
